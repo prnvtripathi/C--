685279efe656bb91eb6ca0bea0a50c8520ae4486
Pranav_Tripathi <tripathipranav14@gmail.com>
2022-09-13 09:35:54 +0530

    13 september 2022

--- DSTL Lab/intersection.c
+++ DSTL Lab/intersection.c
@@ -4,19 +4,19 @@ int main()
     int a[10], b[10], c[20], flag = 0, n1, n2, i, j, k = 0;
     printf("Enter array1 size : ");
     scanf("%d", &n1);
+
     printf("\nEnter array2 size : ");
     scanf("%d", &n2);
+
     printf("\nEnter array1 element : ");
     for (i = 0; i < n1; i++)
         scanf("%d", &a[i]);
+
     printf("\nEnter array2 element : ");
     for (i = 0; i < n2; i++)
         scanf("%d", &b[i]);
-    printf("\nUnion:");
 
-    for (i = 0; i < n1; i++)
-        c[k++] = a[i];
-
+    printf("Intersection: ");
     for (i = 0; i < n2; i++)
     {
         for (j = 0; j < n1; j++)
@@ -26,17 +26,16 @@ int main()
                 flag = 1;
             }
         }
-        if (flag == 0)
+        if (flag == 1)
         {
             c[k++] = b[i];
         }
         flag = 0;
     }
 
-    for (int i = 0; i < k; i++)
+    for (i = 0; i < k; i++)
     {
         printf("%d ", c[i]);
     }
-
     return 0;
 }
