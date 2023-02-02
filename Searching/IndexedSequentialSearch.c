c01eea133a30f500878974cbb19544400b0d842e
Pranav Tripathi <tripathipranav14@gmail.com>
2023-02-03 00:02:52 +0530

    indexed sequential search update

--- Searching/IndexedSequentialSearch.c
+++ Searching/IndexedSequentialSearch.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void indexedSequentialSearch(int arr[], int n, int k)
+int GN = 3; // group number, showing number of elements in a group
+
+void indexedSequentialSearch(int arr[], int n, int target)
 {
-    const int GN = 3;
+
     int elements[GN], indices[GN], i, set = 0;
     int j = 0, ind = 0, start, end;
+
     for (i = 0; i < n; i += 3)
     {
-        elements[ind] = arr[i];
-        indices[ind] = i;
+        elements[ind] = arr[i]; // element stored
+        indices[ind] = i;       // index stored
         ind++;
     }
-    if (k < elements[0])
+
+    if (target < elements[0])
     {
-        printf("Not found");
-        exit(0);
+        printf("Not Found!");
+        return;
     }
+
     else
     {
-        for (i = 1; i <= ind; i++)
-            if (k <= elements[i])
+        for (i = 1; i < ind; i++)
+        {
+            if (target <= elements[i])
             {
                 start = indices[i - 1];
                 end = indices[i];
                 set = 1;
                 break;
             }
+        }
     }
+
     if (set == 0)
     {
         start = indices[GN - 1];
         end = GN;
     }
+
     for (i = start; i <= end; i++)
     {
-        if (k == arr[i])
+        if (target == arr[i])
         {
             j = 1;
             break;
         }
     }
+
     if (j == 1)
         printf("Found at index %d", i);
     else
@@ -53,7 +63,7 @@ int main()
     int arr[] = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    int k = 12;
-    indexedSequentialSearch(arr, n, k);
+    int target = 12;
+    indexedSequentialSearch(arr, n, target);
     return 0;
 }
